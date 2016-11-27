7790c4b46869e0c3a6960980755df8e2f12fd31f
Austin Baum <austin@austinbaum.com>
2016-11-27 10:59:11 -0700

    fix advent calculation

--- API/seasons.php
+++ API/seasons.php
@@ -1,11 +1,31 @@
 <?php
 
+// default timezone 
+date_default_timezone_set("America/Phoenix");
+
 // Date format will be "YYYY-MM-DD"
 $today = date("Y-m-d");
 $this_year = date("Y");
 $this_christmas = date($this_year."-12-25");
-$this_advent = date("Y-m-d", strtotime("last sunday -3weeks", strtotime("tomorrow", strtotime($this_christmas))));
 $last_year = $this_year - 1;
+
+// New Advent Calc
+// Ref: https://www.codeproject.com/articles/10860/calculating-christian-holidays
+function Advent($Y) {
+  $weeks = 4;
+  $correction = 0;
+  $new_christmas = date($Y."-12-25");
+  $christmas_day_int = date("N", strtotime($new_christmas));
+
+    if ($christmas_day_int != 7)
+    {
+        $weeks--;
+        $correction = ($christmas_day_int - 1);
+    }
+    return date("Y-m-d", strtotime(-1 * (($weeks * 7) + $correction)."days", strtotime($new_christmas)));
+}
+$this_advent = Advent($this_year);
+
 
 if($today >= $this_advent) {
 	// We're already in advent for this Calendar
@@ -14,7 +34,7 @@ if($today >= $this_advent) {
 	$this_year = $this_year + 1;
 	$advent_year = $next_year;
 	$next_christmas = date($this_year."-12-25");
-	$next_advent = date("Y-m-d", strtotime("last sunday -3weeks", strtotime("tomorrow", strtotime($next_christmas))));
+	$next_advent = Advent($this_year);
 } else {
 	// We are NOT yet in Advent this Calendar year
 	$last_year = $this_year - 1;
@@ -24,7 +44,7 @@ if($today >= $this_advent) {
 	$next_christmas = $this_christmas;
 	$next_advent = $this_advent;
 	$this_christmas = date($last_year."-12-25");
-	$this_advent = date("Y-m-d", strtotime("last sunday -3weeks", strtotime("tomorrow", strtotime($this_christmas))));
+	$this_advent = Advent($this_year);
 }
 
 // Easter Magic
@@ -57,7 +77,7 @@ function get_seasons($name, $startend, $year)
 	$fn_next_christmas = date($fn_this_year."-12-25");
 	$fn_next_advent = date("Y-m-d", strtotime("last sunday -3weeks", strtotime("tomorrow", strtotime($fn_next_christmas))));
 	$fn_this_christmas = date($fn_last_year."-12-25");
-	$fn_this_advent = date("Y-m-d", strtotime("last sunday -3weeks", strtotime("tomorrow", strtotime($fn_this_christmas))));
+	$fn_this_advent = Advent($fn_last_year);
 
 	// The Season Dates;
 	$advent = date("Y-m-d", strtotime($fn_this_advent) );
@@ -177,58 +197,63 @@ function get_seasons($name, $startend, $year)
   return $return;
 }
 
-/* Test 
-echo "<br/>today = " . $today;
-echo "<br/>this_year = " . $this_year;
-echo "<br/>this_christmas = " . $this_christmas;
-echo "<br/>this_advent = " . $this_advent;
-echo "<br/>last_year = " . $last_year;
-echo "<hr/>";
+ // Test 
+// echo "<br/>today = " . $today;
+// echo "<br/>this_year = " . $this_year;
+// echo "<br/>this_christmas = " . $this_christmas;
+// echo "<br/>this_advent = " . $this_advent;
+// echo "<br/>last_year = " . $last_year;
+// echo "<hr/>";
 
-echo "<br/>next_year = " . $next_year;
-echo "<br/>advent_year = " . $advent_year;
-echo "<br/>next_christmas = " . $next_christmas;
-echo "<br/>next_advent = " . $next_advent;
-echo "<hr/>";
+// echo "<br/>Adevnt func test = " . Advent("2016");
+// echo "<br/>$new_christmas = " . date($this_year."-12-25");
+// echo "<br/>$christmas_day_int = " . date("N", strtotime($this_christmas));
+// echo "<hr/>";
 
-echo "<br/>C = " . $C;
-echo "<br/>N = " . $N;
-echo "<br/>K = " . $K;
-echo "<br/>I = " . $I;
-echo "<br/>J = " . $J;
-echo "<br/>L = " . $L;
-echo "<br/>M = " . $M;
-echo "<br/>D = " . $D;
-echo "<br/>Easter = " . Easter($this_year);
-echo "<br/>Last Easter = " . Easter($last_year);
-echo "<br/>Next Easter = " . Easter($next_year);
+// echo "<br/>next_year = " . $next_year;
+// echo "<br/>advent_year = " . $advent_year;
+// echo "<br/>next_christmas = " . $next_christmas;
+// echo "<br/>next_advent = " . $next_advent;
+// echo "<hr/>";
 
-echo "<hr/>";
+// echo "<br/>C = " . $C;
+// echo "<br/>N = " . $N;
+// echo "<br/>K = " . $K;
+// echo "<br/>I = " . $I;
+// echo "<br/>J = " . $J;
+// echo "<br/>L = " . $L;
+// echo "<br/>M = " . $M;
+// echo "<br/>D = " . $D;
+// echo "<br/>Easter = " . Easter($this_year);
+// echo "<br/>Last Easter = " . Easter($last_year);
+// echo "<br/>Next Easter = " . Easter($next_year);
 
-function showseasons($this_year) {
-	echo "<br/>advent starts = " . get_seasons('advent', 0, $this_year);
-	echo "<br/>advent ends = " . get_seasons('advent', 1, $this_year);
-	echo "<br/>christmas starts = " . get_seasons('christmas', 0, $this_year);
-	echo "<br/>christmas ends = " . get_seasons('christmas', 1, $this_year);
-	echo "<br/>epiphany starts = " . get_seasons('epiphany', 0, $this_year);
-	echo "<br/>epiphany ends = " . get_seasons('epiphany', 1, $this_year);
-	echo "<br/>ordinary1 starts = " . get_seasons('ordinary1', 0, $this_year);
-	echo "<br/>ordinary1 ends = " . get_seasons('ordinary1', 1, $this_year);
-	echo "<br/>lent starts = " . get_seasons('lent', 0, $this_year);
-	echo "<br/>lent ends = " . get_seasons('lent', 1, $this_year);
-	echo "<br/>easter starts = " . get_seasons('easter', 0, $this_year);
-	echo "<br/>easter ends = " . get_seasons('easter', 1, $this_year);
-	echo "<br/>pentecost starts = " . get_seasons('pentecost', 0, $this_year);
-	echo "<br/>pentecost ends = " . get_seasons('pentecost', 1, $this_year);
-	echo "<br/>ordinary2 starts = " . get_seasons('ordinary2', 0, $this_year);
-	echo "<br/>ordinary2 ends = " . get_seasons('ordinary2', 1, $this_year);
-	echo "<br/>";
-	echo "<br/>current season = " . get_seasons('current', 0, $this_year);
-	echo "<br/>current season = " . get_seasons('current', 1, $this_year);
-	echo "<br/>current season = " . get_seasons('current', 2, $this_year);
-}
-showseasons($this_year);
-echo "<hr/>";
-showseasons($next_year);
-*/
+// echo "<hr/>";
+
+// function showseasons($this_year) {
+// 	echo "<br/>advent starts = " . get_seasons('advent', 0, $this_year);
+// 	echo "<br/>advent ends = " . get_seasons('advent', 1, $this_year);
+// 	echo "<br/>christmas starts = " . get_seasons('christmas', 0, $this_year);
+// 	echo "<br/>christmas ends = " . get_seasons('christmas', 1, $this_year);
+// 	echo "<br/>epiphany starts = " . get_seasons('epiphany', 0, $this_year);
+// 	echo "<br/>epiphany ends = " . get_seasons('epiphany', 1, $this_year);
+// 	echo "<br/>ordinary1 starts = " . get_seasons('ordinary1', 0, $this_year);
+// 	echo "<br/>ordinary1 ends = " . get_seasons('ordinary1', 1, $this_year);
+// 	echo "<br/>lent starts = " . get_seasons('lent', 0, $this_year);
+// 	echo "<br/>lent ends = " . get_seasons('lent', 1, $this_year);
+// 	echo "<br/>easter starts = " . get_seasons('easter', 0, $this_year);
+// 	echo "<br/>easter ends = " . get_seasons('easter', 1, $this_year);
+// 	echo "<br/>pentecost starts = " . get_seasons('pentecost', 0, $this_year);
+// 	echo "<br/>pentecost ends = " . get_seasons('pentecost', 1, $this_year);
+// 	echo "<br/>ordinary2 starts = " . get_seasons('ordinary2', 0, $this_year);
+// 	echo "<br/>ordinary2 ends = " . get_seasons('ordinary2', 1, $this_year);
+// 	echo "<br/>";
+// 	echo "<br/>current season = " . get_seasons('current', 0, $this_year);
+// 	echo "<br/>current season = " . get_seasons('current', 1, $this_year);
+// 	echo "<br/>current season = " . get_seasons('current', 2, $this_year);
+// }
+// showseasons($this_year);
+// echo "<hr/>";
+// showseasons($next_year);
+
 ?>
